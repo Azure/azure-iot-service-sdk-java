400dc0b759b851e570da56b615ea397dffaba944
Ethan Ann <110260004+ethanann-ms@users.noreply.github.com>
2023-12-09 23:25:35 -0500

    Add device deletion sample for hub

---
+++ vsts/cleanup.ps1
@@ -0,0 +1,15 @@
+if ($env:IOTHUB_CONNECTION_STRING.length -eq 0)
+{
+    $exception = "Iot hub connection string not set"
+    throw $exception
+}
+
+#Install the Java SDK
+mvn install -DskipTests=true -T 2C
+
+#move to sample folder where you can run the sample that deletes all devices tied to a hub
+cd service/iot-service-samples/device-deletion-sample/target
+
+#Run sample code to delete all devices from these iot hubs
+echo "Cleaning up iot hub"
+java -jar deviceDeletionSample.jar $env:IOTHUB_CONNECTION_STRING
