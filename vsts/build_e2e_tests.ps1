a228f73ea13672f7615fb890a731f6566e04ae31
Ethan Ann <110260004+ethanann-ms@users.noreply.github.com>
2024-01-04 14:23:08 -0500

    Updated the build e2e tests script

--- vsts/build_e2e_tests.ps1
+++ vsts/build_e2e_tests.ps1
@@ -1,1 +1,1 @@
-mvn `-Dmaven.javadoc.skip=true --projects :iot-e2e-common ` --also-make clean install `-DskipTests`
+mvn `-Dmaven.javadoc.skip=true --projects :iot-e2e-common ` --also-make clean install `-DskipTests -DskipUnitTests`
